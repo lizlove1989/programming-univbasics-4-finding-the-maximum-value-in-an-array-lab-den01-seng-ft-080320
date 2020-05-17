c751eff8b70961f56b4deb01047080ef5cc87342
Liz Love <love.elizabeth1989@gmail.com>
2020-05-17 13:36:27 +0000

    Done.

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,9 @@
 def find_max_value(array)
-  # Add your solution here
+  highest_value = 0
+  array.each do |i|
+    if i > highest_value
+      highest_value = i
+    end
+  end
+  highest_value
 end
